ff3712977ff5c1c3c6052c7eccf610ff4c12432e
Diego Elio Pettenò <flameeyes@flameeyes.com>
2023-12-26 17:48:28 +0000

    Use JSX-safe apostrophes and quotes.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -17,11 +17,11 @@ export default function About() {
         </div>
 
         <div className="block">
-          In particular, I've been arguing with many people over the years
+          In particular, I&apos;ve been arguing with many people over the years
           that, despite increasing adoption from the point of view of providers,
-          there's very little interest from the point of view of end users,
+          there&apos;s very little interest from the point of view of end users,
           since outside of big tech, most of the websites people need to visit
-          for their "daily life" do not support IPv6 at all.
+          for their “daily life” do not support IPv6 at all.
         </div>
 
         <div className="block">
@@ -30,38 +30,38 @@ export default function About() {
           employers are interested in IPv6 enough to have public trackers on
           adoption, and this project is totally unrelated to both, measuring
           a completely different metric and <b>it is my personal opinion, not
-            reflecting my employer's</b>.
+            reflecting my employer&apos;s</b>.
         </div>
 
         <h2 className="title is-2">Metric and Measurement</h2>
         <div className="block">
-          Finding IPv6 trackers for ISP adoption is easy (see above), and it's
-          also very easy to find trackers that look for "Alexa Top XX" domains.
+          Finding IPv6 trackers for ISP adoption is easy (see above), and it&apos;s
+          also very easy to find trackers that look for “Alexa Top XX” domains.
           Both of these types of tracking are easy to execute in bulk, because
-          they don't need manual curation at all. On the other hand, I would
-          argue they provide a much less useful picture when it comes to "daily
-          usage".
+          they don&apos;t need manual curation at all. On the other hand, I would
+          argue they provide a much less useful picture when it comes to “daily
+          usage”.
         </div>
 
         <div className="block">
-          What I'm focusing on for this project, is to gather not just domains
+          What I&apos;m focusing on for this project, is to gather not just domains
           and sites, but whole services, and collect them by country. This has
           the unfortunate effect of requiring a lot more manual work, and a lot
-          of country specific knowledge, which is why you'll find the selection
+          of country specific knowledge, which is why you&apos;ll find the selection
           fairly biased towards the countries I have first hand knowledge of.
         </div>
 
         <div className="block">
 
-          The reason why I'm gathering services together, is because it's not
+          The reason why I&apos;m gathering services together, is because it&apos;s not
           uncommon for many services to use different hostnames and hosting
-          providers between their "showroom" website and the site that includes
+          providers between their “showroom” website and the site that includes
           user management. This is to be expected as, for many years now, we
           have been using hostnames (or domain names in some other cases) as
           security boundaries. So for instance, many banks would have one domain
           to show off their current account offers, which would be including
           scripts for analytics and similar, while a completely different domain
-          would be used for online banking, which wouldn't be loading any of that.
+          would be used for online banking, which wouldn&apos;t be loading any of that.
         </div>
       </div>
     </section >
